bb73fc6e295584af287869c1e5ebb4b33f017412
jinyeom <jinyeom95@gmail.com>
2018-09-02 18:35:24 -0500

    initial commit

---
+++ leuchtturm/leuchtturm.py
@@ -0,0 +1,82 @@
+import os
+import logging
+import json
+from copy import deepcopy
+from typing import Sequence
+
+import fire
+import nbformat
+
+
+RC_FILENAME = ".leuchtturmrc.json"
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class RuntimeConfig(object):
+    def __init__(self, rc: dict) -> None:
+        self.author = rc["author"]
+        self.contents = rc["contents"]
+
+    @classmethod
+    def empty(cls):
+        return cls(dict(author="", contents=[]))
+
+    def export(self, path):
+        data = dict(author=self.author, contents=self.contents)
+        with open(path, "w+") as f:
+            f.write(json.dumps(data))
+
+
+class Leuchtturm(object):
+    def __init__(self):
+        self._cwd = os.getcwd()
+        self._rc_path = os.path.join(self._cwd, RC_FILENAME)
+        if not os.path.exists(self._rc_path):
+            RuntimeConfig.empty().export(self._rc_path)
+        with open(self._rc_path, "r") as f:
+            self._rc = RuntimeConfig(json.load(f))
+        
+    def _generate_readme(self) -> None:
+        r"""Generate a new `README.md` file from the list of contents"""
+        raise NotImplementedError
+
+    def _generate_nb(self, nb_dir: str) -> None:
+        r"""Generate a new Jupyter notebook"""
+        title = os.path.basename(nb_dir) # base name of the directory is always the title
+        nb_path = os.path.join(nb_dir, title + ".ipynb")
+        nb = nbformat.v4.new_notebook()
+        nb["cells"] = [nbformat.v4.new_markdown_cell(f"# {title}\n"
+                                                     f"Author: {self._rc.author}"),
+                       nbformat.v4.new_code_cell()]
+        nbformat.write(nb, nb_path)
+
+    def create(self, nb_name: str) -> None:
+        cwd = os.getcwd() # current working directory
+        # create a new directory with the notebook name
+        nb_dir = os.path.join(cwd, nb_name)
+        try:
+            os.mkdir(nb_dir) # may raise `FileExistsError`
+            self._generate_nb(nb_dir)
+        except FileExistsError:
+            logger.error(f"{nb_dir} already exists")
+            return
+        # append the new notebook to the contents
+        self._rc.contents.append(nb_name)
+        self._rc.export(self._rc_path)
+
+    def remove(self, nb_name: str):
+        # TODO: search for `nb_name` in the contents
+        #       if it exists:
+        #           prompt for assurance
+        #               if yes:
+        #                   find the directory and remove it
+        #                   remove it from the contents (overwrite contents)                 
+        raise NotImplementedError("`remove` is not implemented")
+
+
+if __name__ == "__main__":
+    fire.Fire(Leuchtturm)
+
