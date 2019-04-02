0ea5390712cfe2d027d13ed78379ca8d2611c5f4
jinyeom <jinyeom95@gmail.com>
2019-04-02 15:08:02 -0500

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 
-setup(name="leuchtturm",
+setup(name="metis",
       version="0.0.2",
       description="A toolkit for managing a notebook environment",
       author="Jin Yeom",
-      author_email="jinyeom@utexas.edu",
-      scripts=["leuchtturm"],
+      author_email="jin.yeom@hudl.com",
+      scripts=["metis"],
       install_requires=["fire", "nbformat"])
